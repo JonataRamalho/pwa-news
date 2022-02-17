9cb11b39a3737cf4c3adc7c21aefac889b912e7a
Jonata Ramalho <jonataramalho.s@gmail.com>
2022-02-17 20:21:41 -0300

    Feat: Cria componentes para noticias

---
+++ src/containers/components/Economy.jsx
@@ -0,0 +1,41 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import { Row, Col } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const Economy = ({ values }) => {
+  const navigate = useNavigate();
+
+  const renderImg = ({ image, description }) => (
+    <img src={image.url} alt={description} width="100%" />
+  );
+
+  const renderDescription = (description) => (
+    <p dangerouslySetInnerHTML={createMarkup(description)} />
+  );
+
+  const openPost = (id) => {
+    navigate(`/economy/${id}`);
+  };
+
+  const renderPost = () => {
+    const { title, image, description, id } = post;
+
+    return (
+      <Col span={24} md={12} key={`post-${index}`}>
+        <article onClick={() => openPost(id)}>
+          <p>
+            <strong dangerouslySetInnerHTML={createMarkup(title)} />
+          </p>
+          {image?.url
+            ? renderImg({ image, description })
+            : renderDescription(description)}
+        </article>
+      </Col>
+    );
+  };
+
+  return <Row gutter={[16, 16]}>{values?.map(renderPost)}</Row>;
+};
+
+export default Economy;
