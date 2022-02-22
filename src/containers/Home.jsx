34a647f23a481d0ffe29864415a554316af1d166
JonataRamalho <jonataramalho.s@gmail.com>
2022-02-22 12:55:58 -0300

    Fix: Remove md

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -43,7 +43,7 @@ function Home() {
       </Row>
       <hr />
       <Row gutter={[16, 16]}>
-        <Col span={24} md={16}>
+        <Col span={24}>
           <h2>Tecnology</h2>
           <Technology values={news?.technology} />
         </Col>
